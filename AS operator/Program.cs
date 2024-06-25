0df7847c352397f0dae40a56cec13660d3daff40
RockingWithTheBest <151398987+RockingWithTheBest@users.noreply.github.com>
2024-06-25 21:34:50 +0300

    added

---
+++ AS operator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = "Hello";
+            string value = obj as string;
+            Console.WriteLine(value);
+        }
+    }
+}
